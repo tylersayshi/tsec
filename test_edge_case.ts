a22145caec491807a111c525cd9929ae23bbc95f
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-08-09 14:00:00 -0700

    remove llm junk

--- test_edge_case.ts
+++
@@ -1,6 +0,0 @@
-const StringEnum = {
-  X: "x",
-  Y: "y"
-} as const;
-
-type StringEnum = typeof StringEnum[keyof typeof StringEnum];
--- test_script.ts
+++
@@ -1,3 +0,0 @@
-import { runCodemod } from "./main.ts";
-
-runCodemod("test_edge_case.ts");
